c4375f7389b532588c3d1a121f7abbbdd02e6c45
Mateus Oliveira <mtsodf@gmail.com>
2024-10-06 16:28:27 -0300

    Enunciado do primeiro exercício

---
+++ exercicio01.py
@@ -0,0 +1,27 @@
+"""
+Dado a seguinte função abaixo responda as perguntas nos locais indicados nos comentários.
+"""
+
+
+def ler_extensao_do_arquivo(caminho_do_arquivo):
+    extensao = caminho_do_arquivo.split(".")[-1]
+    return extensao
+
+
+# a) Quantos parâmetros são passados para essa função? Qual o nome do parâmetro?
+"""
+Escreva sua resposta aqui
+"""
+
+# b) Qual o retorno da função caso o argumento "tp1.pdf" seja passado para ela?
+"""
+Escreva sua resposta aqui
+"""
+
+# c) Descreva o que essa função faz
+"""
+Escreva sua resposta aqui
+"""
+
+
+# d) Se for passado o argumento "tp1.pdf.backup", qual seria o retorno da função?
