932f80e755bf8c458a2e65ce5e988ae8fefbaffb
Mateus Oliveira <mtsodf@gmail.com>
2024-10-07 17:51:06 -0300

    Mais exercicios

--- exercicio15.py
+++ exercicio15.py
@@ -1,3 +1,17 @@
 """
-Escreva uma função que 
+Escreva uma função para cálculo da mediana e outra função para o cálculo da média.
+A função que calcula a mediana deve receber uma lista de números e retornar a mediana.
+A função que calcula a média deve receber uma lista de números e retornar a média.
+
+A mediana é o valor que separa a metade maior e a metade menor de um conjunto de dados.
+Caso a quantidade de valores seja ímpar a mediana é o valor central, caso contrário é a média dos dois valores centrais.
+
+Por exemplo:
+mediana([1, 2, 3, 4, 5]) -> 3
+mediana([1, 2, 3, 4, 5, 6]) -> 3.5
+
+
+A média é o valor obtido pela soma de todos os valores dividido pela quantidade de valores.
+
+Dica: Para o calculo da mediana, utilize é mais fácil ordenar a lista de números.
 """
---
+++ exercicio16.py
@@ -0,0 +1,24 @@
+"""
+Um carro foi monitorado via gps para testes de desempenho de velocidade.
+Ele viajou entre duas cidades A e B.
+Com isso foram gerados os seguintes dados:
+    - tempos: lista com o duração que o carro ficou em determinada velocidade (em segundos)
+    - velocidades: lista com as respectivas velocidades relativas aos tempos anteriores(em km/h)
+
+Por exemplo,
+tempos = [120, 60]
+velocidades = [80, 100]
+significa que o carro ficou 120 segundos a 80 km/h e 60 segundos a 100 km/h.
+
+Com base nessas listas calcule
+
+a) O tempo de percuso total (em segundos)
+b) A distância entre as duas cidades
+c) A velocidade média do percurso
+"""
+
+import random
+
+tempos = [random.uniform(60, 300) for _ in range(40)]
+velocidades = [random.normalvariate(100, 20) for _ in range(40)]
+velocidades = [v if v > 0 else 0 for v in velocidades]
